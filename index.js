c4a9a30711ac765a70994de99fc292b58072e6e9
Kyle Andrews <andrewskr90@gmail.com>
2021-06-11 14:15:17 -0700

    task 1,2,3,5,7

--- index.js
+++ index.js
@@ -244,8 +244,9 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const artists20 = [];
+
 }
 
 
@@ -259,9 +260,12 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, index){
+   array.splice(index,1);
+   return(array.length);
 }
+console.log('task 5',removeArtist(artists,0));
+
    
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -280,9 +284,16 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push({"id": 20,
+      "name": "Kyle Andrews", 
+      "years": "1990 - present",
+      "genre": "Web Design", 
+      "nationality": "American",
+      "bio": "lorem ipsum"});
+      return artists;
   }
+  console.log('task 6', addArtist(artists));
 
   
 
@@ -293,9 +304,16 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  const hundred = [];
+  for(let i=0;i<array.length;i++){
+    if(array[i]["paintings"]>=100){
+      hundred.push(array[i]["name"]);
+    }
+  }
+  return hundred;
 }
+console.log('task 7', lotsOfArt(artists));
 
 
 
